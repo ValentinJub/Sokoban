118c02289ab402c4c75a2df3c75de4e61f0feee2
Valentin Wissler <valentinwissler42@outlook.com>
2024-02-17 15:13:56 +0000

    first commit

---
+++ editeur.h
@@ -0,0 +1,29 @@
+/*
+editeur.h
+-----
+Par Valentin Wissler
+
+Prototypes pour fonctions dans editeur.c
+*/ 
+
+#ifndef DEF_EDITEUR
+#define DEF_EDITEUR
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include "constantes.h"
+#include "fichier.h"
+#include "jeu.h"
+
+void editeur(SDL_Renderer*);
+
+int manuelEditeur(SDL_Renderer*renderer, TTF_Font*font);
+
+#endif
+
---
+++ test.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+	int x=74, y=10;
+
+	x/=34;
+	printf("x vaut : %d",x);
+	return EXIT_SUCCESS;
+}
